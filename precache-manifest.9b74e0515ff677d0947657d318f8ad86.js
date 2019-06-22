38160cf56d4a9ebb17981aa93fa60a72fa15f87d
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-06-22 14:40:48 +0200

    Updates

--- precache-manifest.9b74e0515ff677d0947657d318f8ad86.js
+++ precache-manifest.9b74e0515ff677d0947657d318f8ad86.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "600b89b2dc452e374353f1f128b112fe",
+    "revision": "4b0bac897d912cb9365238a9b6094726",
     "url": "/index.html"
   },
   {
-    "revision": "d69af68c14e097a2439e",
-    "url": "/static/css/main.40456ffc.chunk.css"
+    "revision": "4b86efa286ed010b8b69",
+    "url": "/static/css/main.5842cfc2.chunk.css"
   },
   {
     "revision": "58f496bc1ce27350f0eb",
     "url": "/static/js/2.98dcc9dd.chunk.js"
   },
   {
-    "revision": "d69af68c14e097a2439e",
-    "url": "/static/js/main.f03e1813.chunk.js"
+    "revision": "4b86efa286ed010b8b69",
+    "url": "/static/js/main.e45bb0c7.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -22,6 +22,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "78afd35308ca2c2c452b75a06c36455e",
     "url": "/static/media/DSC_0287.78afd353.jpg"
+  },
+  {
+    "revision": "5dc94ad65ff4960a7b6f602639fb3c94",
+    "url": "/static/media/about3.5dc94ad6.jpg"
   },
   {
     "revision": "516e404c24239455dc6219fa4f6eb5cb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f989ac91fb2218d082f6a4e396d205a9.js"
+  "/precache-manifest.9b74e0515ff677d0947657d318f8ad86.js"
 );
 
 self.addEventListener('message', (event) => {
